78a298d5cbc12f6363024e700c5d762c266cbbd4
Lucky Jha <jhalucky61@gmail.com>
2025-09-17 18:45:24 +0530

    file added

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,25 @@
+// src/app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth";
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+import Github from "next-auth/providers/github";
 
-const handler = NextAuth(authOptions);
+const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+     Github({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+};
 
-export { handler as GET, handler as POST };
+export const { handlers, signIn, signOut, auth } = NextAuth(authOptions);
 
+// Re-export handlers so Next.js can use them for GET & POST requests
+export const { GET, POST } = handlers;
