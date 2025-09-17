238b0e82006538ae543b6d0c9784007a835c4d2c
Lucky Jha <jhalucky61@gmail.com>
2025-09-17 19:08:06 +0530

    file added

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,8 @@
-// app/api/auth/[...nextauth]/route.ts
-import NextAuth from "next-auth";
+// src/app/api/auth/[...nextauth]/route.ts
 import { authOptions } from "@/lib/auth";
+import NextAuth from "next-auth/next";
 
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
+
