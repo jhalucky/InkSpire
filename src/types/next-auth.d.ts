5e7ee9dae92fa73c4434fdcb7fbc46b2a363cd39
Lucky Jha <jhalucky61@gmail.com>
2025-09-22 13:23:44 +0530

    file added

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,3 +1,4 @@
+// src/types/next-auth.d.ts
 import NextAuth from "next-auth";
 
 declare module "next-auth" {
@@ -12,9 +13,24 @@ declare module "next-auth" {
       profession?: string | null;
       education?: string | null;
       twitterUrl?: string | null;
+      instagramUrl?: string | null;
       linkedinUrl?: string | null;
       githubUrl?: string | null;
-      instagramUrl?: string | null;
     };
+  }
+
+  interface User {
+    id: string;
+    name: string;
+    email: string;
+    image?: string | null;
+    username?: string | null;
+    bio?: string | null;
+    profession?: string | null;
+    education?: string | null;
+    twitterUrl?: string | null;
+    instagramUrl?: string | null;
+    linkedinUrl?: string | null;
+    githubUrl?: string | null;
   }
 }
